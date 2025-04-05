0ebf934d60e89e0d2a7a988b78ed1c35314adb05
Lukas Koeller <acc.koeller@protonmail.com>
2025-04-05 13:09:56 +0200

    feat: add base styles to layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import "$lib/styles/theme.css"
+    // import "@nordcode/ui/utils/media"
+    import "@nordcode/ui/bundle"
+    import "@nordcode/ui/utils/layouts"
+    import "@nordcode/ui/components/card"
+    import "@nordcode/ui/components/forms"
+    import "@nordcode/ui/components/inputs"
+    import "@nordcode/ui/components/buttons"
+    import "$lib/styles/overrides.css"
+
+    let { children } = $props();
+</script>
+
+<main>
+    {@render children()}
+</main>
