0def48faa77993b7c79e7e86bb922bd14b42f8f4
Lukas Koeller <acc.koeller@protonmail.com>
2025-04-05 13:08:52 +0200

    chore: add lightningcss config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,20 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import browserslist from 'browserslist';
+import { browserslistToTargets } from 'lightningcss';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	css: {
+		transformer: 'lightningcss',
+		lightningcss: {
+			targets: browserslistToTargets(browserslist('>= 0.25%')),
+			drafts: {
+				customMedia: true
+			}
+		}
+	},
+	build: {
+		cssMinify: 'lightningcss'
+	}
 });
