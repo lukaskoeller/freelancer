cb5a660f3132644a7dd1d95241a45cf7d706edcd
Lukas Koeller <acc.koeller@protonmail.com>
2025-04-05 13:09:03 +0200

    feat: add utils for formatting

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,11 +2,30 @@
 
 export type FormatCurrencyOptions = { locales: Intl.LocalesArgument } & Intl.NumberFormatOptions;
 
-export const formatCurrency = (
-	value: number,
-	{ locales, ...options }: FormatCurrencyOptions = {
-		locales: 'de-DE',
+export const formatCurrency = (value: number, options?: FormatCurrencyOptions) =>
+	new Intl.NumberFormat(options?.locales ?? 'de-DE', {
 		style: 'currency',
-		currency: 'EUR'
-	}
-) => new Intl.NumberFormat(locales, options).format(value);
+		currency: 'EUR',
+		...options
+	}).format(value);
+
+export type FormatNumberOptions = { locales: Intl.LocalesArgument } & Intl.NumberFormatOptions;
+
+export const formatNumber = (value: number, options?: FormatNumberOptions) =>
+	new Intl.NumberFormat(options?.locales ?? 'de-DE', {
+		maximumSignificantDigits: 1,
+		...options
+	}).format(value);
+
+export type FormatUnitOptions = { locales: Intl.LocalesArgument } & Intl.NumberFormatOptions;
+
+export const formatUnit = (
+	value: number,
+	unit: Intl.NumberFormatOptions['unit'],
+	options?: FormatUnitOptions
+) =>
+	new Intl.NumberFormat(options?.locales ?? 'de-DE', {
+		style: 'unit',
+		...options,
+		unit
+	}).format(value);
